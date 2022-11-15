fd6dab992fc075c151d9d4b4eb6b303f2c4a2999
KarimovFreedlee <45467077+KarimovFreedlee@users.noreply.github.com>
2022-11-15 15:45:56 +0300

    add basic logic

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import './App.css';
+import Character from "./mechanics/Character"
+import FightMechanic from "./mechanics/FightMechanic"
+import React from 'react';
+import { Npc } from './mechanics/Npc';
+
+const mainCharacter = new Character(25, 5, {min: 10, max: 25})
+const enemyCharacter = new Npc(15, 3, {min:20, max: 30})
+const characters: Character[] = [mainCharacter, enemyCharacter]
+const fightScene = new FightMechanic( characters )
+
+function App() {
+
+  const [hp, setHp] = React.useState(enemyCharacter.getHp())
+
+  const decHp = () => {
+    mainCharacter.dealDamage(enemyCharacter)
+    setHp(enemyCharacter.getHp())
+  }
+
+  return (
+    <div className="App">
+        {hp}
+        <button onClick={() => decHp() }>attack</button>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/mechanics/Npc.tsx
@@ -0,0 +1,10 @@
+import Character, { IAttack } from "./Character";
+
+export class Npc extends Character {
+    private isNpc: boolean
+
+    constructor(initHp: number, initArmor: number, initAttack: IAttack) {
+        super(initHp, initArmor, initAttack)
+        this.isNpc = true
+    }
+}
