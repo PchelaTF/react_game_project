e8b9d0e67348aa47337216ea7438032c81900cc7
KarimovFreedlee <45467077+KarimovFreedlee@users.noreply.github.com>
2022-11-23 21:54:46 +0300

    fightscene reset added

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,20 @@ import './App.css';
 import CreateCharacter from './components/CreateCharacter/CreateCharacter';
 import FightScene from './components/FIghtScene/FightScene';
 import MainScene from './components/MainScene/MainScene';
-import { useAppSelector } from './store/store';
+import { useAppDispatch, useAppSelector } from './store/store';
 import fightSceneImg from "./assets/img/War2.png"
 import { createEnemy } from './mechanics/CreatingMechanic';
+import { fightSlice } from './store/reducers/FightReducer';
 
 function App() {
   const scene = useAppSelector(state => state.SceneReducer.scene)
   const mainCharacter = useAppSelector(state => state.userReducer.character)
+  const dispatch = useAppDispatch()
+  const { clearDeadEnemies } = fightSlice.actions
+
+  React.useEffect(() => {
+    dispatch(clearDeadEnemies())
+  },[scene])
 
   const getScene = () => {
     switch (scene) {
